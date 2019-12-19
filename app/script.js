d47bdd6c8c8f793cb9ad3ee93a86c73caa1054dd
Ola Siem <info@siemieniuk.net>
2019-12-19 19:43:50 +0100

    Fix all timers

--- app/script.js
+++ app/script.js
@@ -5,70 +5,90 @@ class App extends React.Component {
   state = {
     status: 'off',
     time: 1200, // 20 min counter
-    timer: null, // 20 sec interval
+    timer: null, // 1 sec interval
   }
 
   formatTime = (time) => {
+    let minutes, seconds;
+
     if (typeof time != 'number' || time <0) {
       return null;
     }
-      
-    let hours, minutes;
-    const h = parseInt(time / 3600); // 1h
-  
-    if (h <= 0) {
-      hours = '00';
-    } else if (h < 10) {
-      hours = `0${h}`;
-    } else {
-      hours = h;
-    }
-  
-    time -= h * 3600;
-    const m = parseInt(time / 60);
+
+    const m = parseInt(time / 60); // 1 min
   
     if (m <= 0) {
       minutes = '00';
     } else if (m < 10) {
-      minutes = `0${m}`;
+      minues = `0${h}`;
     } else {
       minutes = m;
     }
   
-    return `${hours} : ${minutes}`;
+    time -= minutes * 60;
+  
+    if (time <= 0) {
+      seconds = '00';
+    } else if (time < 10) {
+      seconds = `0${m}`;
+    } else {
+      seconds = time;
+    }
+  
+    return `${minutes} : ${seconds}`;
   };
 
   changeStatusHandler(newStatus) {
     this.setState({ 
       status: newStatus, 
     });
+
+    if (newStatus == 'rest') {
+      this.setState({
+        status: 'rest',
+        time: 20,
+      });
+    }
   };
 
   step = () => {
-    // Remove one second, set state so a re-render happens.
-    let seconds = this.state.seconds - 1;
+    // remove one second, set state so a re-render happens
     this.setState({
-      time: this.secondsToTime(seconds),
-      seconds: seconds,
+      time: this.state.time -1,
     });
 
-    // Check if we're at zero
-    if (seconds == 0) {
-      clearInterval(this.timer);
+    // check if we're at zero
+    if (this.state.time == 0) {
+      if (this.state.status === 'work') {
+        this.setState({
+          status: 'rest',
+          time: 20,
+        });
+      } else if (this.state.status === 'rest') {
+        this.setState({
+          status: 'work',
+          time: 1200,
+        });
+      }
     }
   };
 
   startTimer = () => {
-  
     this.setState({
       timer: setInterval(this.step, 1000),
     });
-  
   };
+
+  stopTimer = () => {
+    clearInterval(this.state.timer);
+    this.setState({
+      time: 1200,
+      status: 'off',
+    });
+  }
 
   render() {
     const {status} = this.state;
-
     return (
       <div>
         <h1>Protect your eyes</h1>
@@ -76,10 +96,10 @@ class App extends React.Component {
         <p>This app will help you track your time and inform you when it's time to rest.</p></div>}
         {(status === 'work') && <img src="./images/work.png" />}
         {(status === 'rest') && <img src="./images/rest.png" />}
-        {(status !== 'off') && <div className="timer">{ this.state.time }</div>}  
+        {(status !== 'off') && <div className="timer">{this.formatTime(this.state.time)}</div>}  
         {(status === 'off') && <button className="btn" onClick={ event => {this.changeStatusHandler('work'); this.startTimer() }}>Start</button>}
         {(status !== 'off') && <button className="btn" onClick={ event => this.changeStatusHandler('rest')}>Stop</button>}
-        <button className="btn btn-close">X</button>
+        <button className="btn btn-close" onClick={ event => {this.stopTimer()} }>X</button>
       </div>
     )
   }
