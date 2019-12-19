6072314168b5be26b655e489a64d27746a254a67
Ola Siem <info@siemieniuk.net>
2019-12-19 09:28:07 +0100

    Add event listeners to buttons

--- app/script.js
+++ app/script.js
@@ -42,21 +42,27 @@ class App extends React.Component {
     this.setState({ status: newState });
   }
 
+  step = () => {};
+
+  startTimer = () => {
+    this.setState({ 
+      timer: setInterval(this.step, 1000),
+    });
+  };
+
   render() {
     const { status } = this.state;
 
     return (
       <div>
         <h1>Protect your eyes</h1>
-        <img src="./images/work.png" />
-        <img src="./images/rest.png" />    
         {(status === 'off') && <div className="description"><p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
         <p>This app will help you track your time and inform you when it's time to rest.</p></div>}
         {(status === 'work') && <img src="./images/work.png" />}
         {(status === 'rest') && <img src="./images/rest.png" />}
-        {(status !== 'off') && <div className="timer">{formatTime()}</div>}
-        {(status === 'off') && <button className="btn" onClick={ event =>this.handleChangeStatus('work') }>Start</button>}
-        {(status !== 'off') && <button className="btn">Stop</button>}
+        {(status !== 'off') && <div className="timer">{ this.state.time }</div>}
+        {(status === 'off') && <button className="btn" onClick={ event => this.handleChangeStatus('work') }>Start</button>}
+        {(status !== 'off') && <button className="btn" onClick={ event => this.handleChangeStatus('rest') }>Stop</button>}
         <button className="btn btn-close">X</button>
       </div>
     )
